2dff468bb8ca3462d9c4f64186b185b56f429796
Sharuf shaik <shaiksharuf95@gmail.com>
2022-12-10 14:22:59 +0530

    javascript-basics part one

---
+++ basics/strings.js
@@ -0,0 +1,25 @@
+`Im from \"Ongole\"`
+"Name : Sharuf City: Ongole Designation: Dev"
+`Name : Sharuf 
+ City: Ongole
+ Designation: Dev`
+
+ "Name :Sharuf\"Full_Stack_Dev\""
+'Name :Sharuf"Full_Stack_Dev"'
+> 999999999999+1
+1000000000000
+> 5+5+1
+11
+> "5 plus 5 is (5+5)"
+'5 plus 5 is (5+5)'
+> `5 plus 5 is {5+5}`
+'5 plus 5 is {5+5}'
+
+`5 plus 5 is ${5+5}`
+'5 plus 5 is 10'
+> "Im from \"USA\""
+'Im from "USA"'
+> "Im from \"CANADA\""
+'Im from "CANADA"'
+> 'Im from \"England\"'
+'Im from "England"'
