209c027fc12aac9b59127bd0fe1d72dfc990a1fd
Sharuf shaik <shaiksharuf95@gmail.com>
2022-12-12 13:16:02 +0530

    basics part 3 added

---
+++ basics/008.Nan.js
@@ -0,0 +1,19 @@
+// Nan Stands for Not A Number
+'Shaik' * 'Sharuf'; // Nan
+
+NaN == NaN ; // false
+NaN === NaN ; // fasle
+
+Number.isNaN(NaN); //true
+Number.isNaN(2*1+'Syed'*45); // true
+   // 2 + 1 + 'Shaik'+ 45 
+  // 2 + 'Shaik'* 45
+ //2 + NaN
+// NaN
+Number.is NaN(1000);     // False
+Number.is NaN(56 * 99); // False
+Number.is NaN("Shaik");// False
+Number.is NaN(true);  // False
+Number.is NaN(NaN);  // true
+
+'3' + 5 // '35'
---
+++ basics/009.implicit-and-explicit.js
@@ -0,0 +1,18 @@
+// Implicit Conversation(Conversation done automatically by javascript Complier & Engine)
+100 == "100"; // true -> (100 == "100"->"100" == "100" -> true)
+
+// Implicit conversationt to a string
+'25' + 15; // '2515'
+
+Implicit conversation to a Number
+23 * '2'; // 46
+23 - true; // 22// true is converted into 1
+true - Null ; // 1 
+false * undefined; // NaN // Udefined into NaN
+
+conversation to boolean Explicity
+Number('23') + 7; // 30
+
+conversation to boolean Explicity
+Boolean (''); // false
+Boolean (2) ==true; // true
