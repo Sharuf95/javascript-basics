4442d0ac1dc3dde13a9d36f07fe55d433d28c490
Sharuf shaik <shaiksharuf95@gmail.com>
2022-12-18 21:54:55 +0530

    4.first-program

---
+++ 4.first-program/greet-user.js
@@ -0,0 +1,19 @@
+console.log("hello users");
+
+var firstName = "Shaik ";
+var lastName  = "Sharuf";
+
+console.log (`${firstName} ${lastName}`);
+
+console.log("**************************");
+console.log("**************************");
+console.log(`2*1= ${2*1}`);
+console.log(`2*2= ${2*2}`);
+console.log(`2*3= ${2*3}`);
+console.log(`2*4= ${2*4}`);
+console.log(`2*5= ${2*5}`);
+console.log(`2*6= ${2*6}`);
+console.log(`2*7= ${2*7}`);
+console.log(`2*8= ${2*8}`);
+console.log(`2*9= ${2*9}`);
+console.log(`2*10=${2*10}`);
---
+++ 4.first-program/index.js
@@ -0,0 +1,30 @@
+var initial = "Shaik";
+let final = "Sharuf";
+
+const city = "Hyderabad";
+
+let friends = [
+    'Deva',
+    'Anil'
+]
+
+const designation = "Full_Stack_Dev"
+
+
+const myInfo = {
+    fullname    : `${initial} ${final}`,
+    city        :city,
+    friends     : friends,
+    designation : designation
+}
+
+console.log(myInfo);
+
+
+// Javascript Runtime Environment
+// 1. Browser
+// 2. Node
+
+// Run this file in NODE environment
+// example: node FILENAME
+// node index.js
