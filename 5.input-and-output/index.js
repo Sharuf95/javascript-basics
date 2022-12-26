feaeaeac07cd369f6509e9ac206697ea101221e1
Sharuf shaik <shaiksharuf95@gmail.com>
2022-12-26 18:48:38 +0530

    functions added

--- 5.input-and-output/index.js
+++ 5.input-and-output/index.js
@@ -1,11 +1,11 @@
 const readline = require('readline-sync');
 
 
-// Output welcome message
-console.log("Welcome to input and output program");
+// Output welcome message 
+console.log("Welcome to input and output program"); // Heading
 
 // ask user name from the input question
 
 const username = readline.question("please specify your name : ");
 
-console.log(`hello ${username}, welcome to our program`);
+console.log(`hello ${username}, welcome to our program`); 
