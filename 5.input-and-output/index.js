be48729077aab8e16b92797208ddcf0dcddf5e7a
Sharuf shaik <shaiksharuf95@gmail.com>
2023-02-14 14:52:41 +0530

    Input and Output

--- 5.input-and-output/index.js
+++ 5.input-and-output/index.js
@@ -9,3 +9,18 @@ console.log("Welcome to input and output program"); // Heading
 const username = readline.question("please specify your name : ");
 
 console.log(`hello ${username}, welcome to our program`); 
+
+
+console.log("Class__Notes");
+
+/* 
+const readline = require('readline-sync');
+
+// Output welcome message
+console.log("Welcome to input and output program");
+
+// Ask user name from the input question
+const username = readline.question("Please specify your name: ");
+
+console.log(`Hello ${username}, Welcome to our program`);
+*/
