fffbbc626537ed7c8ced24deadafd38e7b40b9ad
Sharuf shaik <shaiksharuf95@gmail.com>
2023-02-14 15:04:02 +0530

    User_resume

--- 6.user-resume/index.js
+++ 6.user-resume/index.js
@@ -29,4 +29,43 @@ const resume = {
    console.log (resume);
 
 
+   console.log("Class__Notes");
+
+   /*
+const readline = require('readline-sync'); // es5
+
+
+// "name": "Syed Zakeer",
+
+/*
+"label": "Programmer",
+"image": "",
+"email": "john@gmail.com",
+"phone": "(912) 555-4321",
+"url": "https://johndoe.com",
+
+console.log("**** Resume Maker **** ");
+
+const name = readline.question('Name: ');
+const label = readline.question("Designation: ");
+const email = readline.question("Email: ");
+const phone = readline.question("Phone: ");
+const url = readline.question("Webiste: ");
+
+const resume = {
+  basics: {
+    name: name,
+    label: label,
+    email: email,
+    phone: phone,
+    url: url
+  }
+};
+
+console.log("User Details")
+console.log(resume)
+   
+*/
+
+
 
--- 6.user-resume/sum.js
+++ 6.user-resume/sum.js
@@ -12,3 +12,24 @@ console.log(`value of ${c} is ${typeof c}`);
 const Sum =  a+b+c;
 
 console.log (`Sum of ${a} + ${b} + ${c} = Sum ${Sum} `);
+
+
+console.log("Class__Notes");
+
+/*
+const readline = require('readline-sync');
+
+console.log("Sum of Two Numbers");
+
+// COnvert value to number
+// 1. Number(value) 
+// 2. +value
+
+const a = +readline.question('Please enter the value of a: ');
+const b = Number( readline.question('Please enter the value of b: ') );
+console.log(`Value of ${a} is ${typeof a}`);
+console.log(`Value of ${b} is ${typeof b}`);
+const sum = a+b;
+
+console.log(`SUM OF ${a} plus ${b} = ${sum}`);
+*/
