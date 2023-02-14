813423a4607d472194d7d8ea2ce432a6e4c906ae
Sharuf shaik <shaiksharuf95@gmail.com>
2023-02-14 14:48:27 +0530

    first program

--- 4.first-program/greet-user.js
+++ 4.first-program/greet-user.js
@@ -17,3 +17,27 @@ console.log(`2*7= ${2*7}`);
 console.log(`2*8= ${2*8}`);
 console.log(`2*9= ${2*9}`);
 console.log(`2*10=${2*10}`);
+
+console.log("Class__Notes");
+
+/*
+console.log("Hello User");
+
+var firstName = "Adil";
+var lastName = "Khan";
+
+console.log(`${firstName} ${lastName}`);
+
+console.log("**************************")
+console.log(`2 x 1 = ${2*1}`);
+console.log(`2 x 2 = ${2*2}`);
+console.log(`2 x 3 = ${2*3}`);
+console.log(`2 x 4 = ${2*4}`);
+console.log(`2 x 5 = ${2*5}`);
+console.log(`2 x 6 = ${2*6}`);
+console.log(`2 x 7 = ${2*7}`);
+console.log(`2 x 8 = ${2*8}`);
+console.log(`2 x 9 = ${2*9}`);
+console.log(`2 x 10 = ${2*10}`);
+console.log("**************************")
+*/
--- 4.first-program/index.js
+++ 4.first-program/index.js
@@ -28,3 +28,37 @@ console.log(myInfo);
 // Run this file in NODE environment
 // example: node FILENAME
 // node index.js
+
+
+console.log("Class__Notes");
+
+/*
+var intial = "Syed";
+let firstName = "Zakeer";
+let lastName = "Hussain";
+
+const city = "Guntur";
+
+const friends = [
+  'Harish',
+  'Harsha',
+  'Mani'
+];
+
+const myDetails = {
+  fullname: `${firstName} ${lastName}, ${intial}`,
+  designation: 'Frontend Dev',
+  friends: friends,
+  city: city
+}
+
+console.log(myDetails);
+
+// Javascript Runtime Environment
+// 1. Browser
+// 2. Node
+
+// Run this file in NODE environment
+// example: node FILENAME
+// node index.js
+*/
